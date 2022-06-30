cb61ee46f77bba665f4b61aceae6f3afd8ef27ba
Roman Nguyen <march08@outlook.com>
2022-06-30 11:18:16 +0200

    cleanup

--- src/components/Img.svelte
+++ src/components/Img.svelte
@@ -1,7 +1,6 @@
 <script lang="ts">
   import { getImageUrl } from "../imageUrl";
   let { src, class: clz, ...rest } = $$restProps;
-  console.log(clz, rest);
 </script>
 
 <img src={getImageUrl(src)} class={clz} alt="" {...rest} />
