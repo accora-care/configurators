6df628983ebc14a9f3493c1bc9eb979418b0b38e
Nazarii Nachas <nazariinachas1@outlook.com>
2024-05-31 12:15:38 +0900

    Convert identation to spaces

--- src/floorbed1-uk/configStore.ts
+++ src/floorbed1-uk/configStore.ts
@@ -2,30 +2,30 @@
 import { writable } from "svelte/store";
 
 export type SELECTOR_VIEW =
-	| "SIDE_RAILS"
-	| "SAFETY"
-	| "MOBILITY"
-	| "EXTRAS"
-	| "MATTRESSES";
+  | "SIDE_RAILS"
+  | "SAFETY"
+  | "MOBILITY"
+  | "EXTRAS"
+  | "MATTRESSES";
 
 interface StoreValueOption {
-	name: string;
-	code: string;
+  name: string;
+  code: string;
 }
 
 interface SideRailStoreValueOption extends StoreValueOption {
-	name: SideRailStoreValue,
-	code: string;
+  name: SideRailStoreValue,
+  code: string;
 };
 
 interface SafetyMatStoreValueOption extends StoreValueOption {
-	name: SafetyMatStoreValue,
-	code: string;
+  name: SafetyMatStoreValue,
+  code: string;
 };
 
 interface LeverStoreValueOption extends StoreValueOption {
-	name: LeverStoreValue,
-	code: string;
+  name: LeverStoreValue,
+  code: string;
 };
 
 export type SideRailStoreValue = "Fabric Side Rails" | "Integrated Junior Padded Side Rails" | "None";
@@ -34,39 +34,39 @@ export type LeverStoreValue = "Bed Lever" | "Short Bed Lever" | "None";
 export type MattressesValue = "Allevia Comfort" | "Allevia Comfort FirmEdge" | "Allevia Duo" | "Allevia Duo Plus" | "Allevia Sense" | "None";
 
 export interface StoreValues {
-	sideRails: SideRailStoreValue;
-	bumpers: boolean;
-	safetyMat: SafetyMatStoreValue;
-	safetySleeve: boolean;
-	juniorKit: boolean;
-	lever: LeverStoreValue;
-	liftingPole: boolean;
-	pumpHolder: boolean;
-	mattressInfill: boolean;
-	bedExtension: boolean;
-	mattressExtensionFoam: boolean;
-	bedExtensionKit: boolean;
-	mattresses: MattressesValue;
-	pumpOption: 'withPump' | 'withoutPump';
-	selectorView: SELECTOR_VIEW | null;
+  sideRails: SideRailStoreValue;
+  bumpers: boolean;
+  safetyMat: SafetyMatStoreValue;
+  safetySleeve: boolean;
+  juniorKit: boolean;
+  lever: LeverStoreValue;
+  liftingPole: boolean;
+  pumpHolder: boolean;
+  mattressInfill: boolean;
+  bedExtension: boolean;
+  mattressExtensionFoam: boolean;
+  bedExtensionKit: boolean;
+  mattresses: MattressesValue;
+  pumpOption: 'withPump' | 'withoutPump';
+  selectorView: SELECTOR_VIEW | null;
 };
 
 export const initVal: StoreValues = {
-	sideRails: "None",
-	bumpers: false,
-	safetyMat: "None",
-	safetySleeve: false,
-	juniorKit: false,
-	lever: "None",
-	liftingPole: false,
-	pumpHolder: false,
-	mattressInfill: false,
-	bedExtension: false,
-	mattressExtensionFoam: false,
-	bedExtensionKit: false,
-	mattresses: "None",
-	pumpOption: 'withPump',
-	selectorView: null,
+  sideRails: "None",
+  bumpers: false,
+  safetyMat: "None",
+  safetySleeve: false,
+  juniorKit: false,
+  lever: "None",
+  liftingPole: false,
+  pumpHolder: false,
+  mattressInfill: false,
+  bedExtension: false,
+  mattressExtensionFoam: false,
+  bedExtensionKit: false,
+  mattresses: "None",
+  pumpOption: 'withPump',
+  selectorView: null,
 };
 
 export const configStore = writable(initVal);
