afd21ad0a382d19fb8d041749244a4775b4ddd2b
Nazarii Nachas <nazariinachas1@outlook.com>
2024-06-17 14:55:28 +0900

    Pressure care options exception

--- src/configura-comfort-uk/isOptionAllowed.ts
+++ src/configura-comfort-uk/isOptionAllowed.ts
@@ -1,9 +1,18 @@
 import type { StoreValues } from "./configStore";
 
+export const getPressureCareOptionsException = (state: StoreValues) => {
+  if (state.chairType === "Configura Comfort Extra Small") {
+    return "Not available with Configura Comfort Extra Small.";
+  }
+
+  return null;
+
+}
+
 export const getLateralSupportBackrestsException = (state: StoreValues) => {
-	if ("None" !== state.lateralSupport) {
+  if ("None" !== state.lateralSupport) {
     return "Not available with Lateral Support Wedges.";
-	}
+  }
 
   return null;
 };
